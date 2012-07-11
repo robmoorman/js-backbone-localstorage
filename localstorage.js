81de86d74614dcbe72c1779031861bff0870ff8c
Rob Moorman <rob@moori.nl>
2012-07-11 15:15:15 +0200

    clear local storage on limit reached

--- localstorage.js
+++ localstorage.js
@@ -23,8 +23,15 @@
                                 var success = options.success;
                                 
                                 options.success = function( response, status, xhr ) {
-                                    localStorage.setItem( id, JSON.stringify( response ));
-                                    localStorage.setItem( id + ":timestamp", new Date().getTime());
+                                    try {
+                                        localStorage.setItem( id, JSON.stringify( response ));
+                                        localStorage.setItem( id + ":timestamp", new Date().getTime());
+                                    }
+                                    catch( error ) {
+                                        if( error == QUOTA_EXCEEDED_ERR ) {
+                                            localStorage.clear();
+                                        }
+                                    }
                                     
                                     if( success ) {
                                         success.apply( this, arguments );
