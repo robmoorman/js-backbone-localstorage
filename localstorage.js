445a2f41b255b56806001abd85a2ff81ea80fbf2
Rob Moorman <rob@moori.nl>
2012-07-11 15:42:24 +0200

    added reset method to clear storage of all with prefix

--- localstorage.js
+++ localstorage.js
@@ -3,6 +3,25 @@
  */
 ( function() {
     
+    var _version = "";
+    var _prefix = "unknown:";
+    
+    function set( key, value ) {
+        return localStorage.setItem( _prefix + key, JSON.stringify( value ));
+    };
+    
+    function get( key ) {
+        return JSON.parse( localStorage.getItem( _prefix + key ));
+    };
+    
+    function reset() {
+        for( var prop in localStorage ) {
+            if( prop.indexOf( _prefix ) === 0 ) {
+                return localStorage.removeItem( prop );
+            }
+        }
+    };
+    
     if( Modernizr.localstorage ) {
         var fallback = Backbone.sync;
         
@@ -16,7 +35,7 @@
                 case "read":
                     if( typeof this.localStorage == "object" ) {
                         var id = this.localStorage.id ? this.localStorage.id : model.id;
-                        var timestamp = window.localStorage.getItem( id + ":timestamp" );
+                        var timestamp = get( id + ":timestamp" );
                         
                         if( id ) {
                             if( options.forceRefresh || !timestamp || ( this.localStorage.maxRefresh && ((( new Date().getTime()) - timestamp ) > this.localStorage.maxRefresh ))) {
@@ -24,12 +43,12 @@
                                 
                                 options.success = function( response, status, xhr ) {
                                     try {
-                                        localStorage.setItem( id, JSON.stringify( response ));
-                                        localStorage.setItem( id + ":timestamp", new Date().getTime());
+                                        set( id, response );
+                                        set( id + ":timestamp", new Date().getTime());
                                     }
                                     catch( error ) {
                                         if( error == QUOTA_EXCEEDED_ERR ) {
-                                            localStorage.clear();
+                                            reset();
                                         }
                                     }
                                     
@@ -41,10 +60,10 @@
                                 fallback.apply( this, [ method, model, options ]);
                             }
                             else {
-                                var data = localStorage.getItem( id );
+                                var data = get( id );
                                 
                                 setTimeout( function() {
-                                    options.success( JSON.parse( data ), "success", null );
+                                    options.success( data, "success", null );
                                 }, 0 );
                             }
                         }
@@ -66,8 +85,10 @@
     window.BackboneLocalStorage = {
         
         setVersion: function( value ) {
+            _version = value;
+            
             if( Modernizr.localstorage ) {
-                var version = localStorage.getItem( "localstorage:version" );
+                var version = get( "version" );
                 
                 if( version && version.toString() != value ) {
                     localStorage.clear();
@@ -75,8 +96,12 @@
                     console.log( "Local Storage is cleared since a different version is detected (from " + version  + " to " + value + ")!" );
                 }
                 
-                localStorage.setItem( "localstorage:version", value.toString());
+                set( "version", value.toString());
             }
+        },
+        
+        setPrefix: function( value ) {
+            _prefix = value + ":";
         }
         
     };
