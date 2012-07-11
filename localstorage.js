b44ba77209f9b95b2e4d5f65ac4e6b5632cf4d37
Rob Moorman <rob@moori.nl>
2012-07-11 14:50:38 +0200

    added versionnumber for clearing localStorage

--- localstorage.js
+++ localstorage.js
@@ -56,4 +56,22 @@
         console.warn( "Local Storage is not supported!" );
     }
     
+    window.BackboneLocalStorage = {
+        
+        setVersion: function( value ) {
+            if( Modernizr.localstorage ) {
+                var version = localStorage.getItem( "localstorage:version" );
+                
+                if( version && version.toString() != value ) {
+                    localStorage.clear();
+                    
+                    console.log( "Local Storage is cleared since a different version is detected (from " + version  + " to " + value + ")!" );
+                }
+                
+                localStorage.setItem( "localstorage:version", value.toString());
+            }
+        }
+        
+    };
+    
 })( window );
